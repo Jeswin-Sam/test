44743549a3be0c808e8f6afcd7c27e4f834fdbdd
Jeswin Sam <jeswinsam.sundar@gmail.com>
2024-04-29 13:02:43 +0530

    added feature

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('myButton').addEventListener('click', function() {
     document.getElementById('message').textContent = 'You clicked the button!';
 });
+
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = randomBgColor();
+});
+
+function randomBgColor() {
+    // Generate random color in hex format
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
